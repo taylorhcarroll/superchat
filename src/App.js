b0526b8005b5428b853fb755a259478ad6f0e570
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-04-19 12:43:39 -0500

    added form submssion for msg

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks';
+import { dblClick } from '@testing-library/user-event/dist/click';
 
 //probably don't have to be this paranoid with firebase but hey, never hurts to be extra safe
 const config = require('./config.json');
@@ -62,12 +63,43 @@ function ChatRoom() {
   //listens to data with hook, reacts to changes in realtime
   const [messages] = useCollectionData(query, { idField: 'id' });
 
+  //stateful value for the form component, default value is empty string
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async (e) => {
+    //prevents submit from refreshing page
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    //creates new document in firestore, note this is not a relational db, it's document based
+    //takes js object as argument, with values you want to write to db
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    //after submission, clears out the input field for user
+    setFormValue('');
+  }
+
   return (<>
     <main>
 
       {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
 
     </main>
+    {/* writes value to firestore */}
+    <form onSubmit={sendMessage}>
+
+      {/* listens to the event anytime the value of input field changes, binds state to form input */}
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say hello!" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
   </>)
 }
 
